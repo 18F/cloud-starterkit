fc7d845662fc8e5a0b6d64c5623010e8540839c3
Peter Burkholder <peter.burkholder@gsa.gov>
2019-01-07 11:33:51 -0500

    No longer syntax err

--- inspec-starterkit/controls/example.rb
+++ inspec-starterkit/controls/example.rb
@@ -12,9 +12,21 @@ control 'azure core resources existence' do
   end
 
   describe azurerm_storage_account_blob_container(
-    resource_group: resource_group, storage_account_name: 'starterkitstorage'
+      resource_group: 'rg', 
+      storage_account_name: 'default', 
+      blob_container_name: 'logs'
   ) do
     it { should exist }
+    its('name') { should eq('logs') }
+  end
+
+  describe azurerm_storage_account_blob_container(
+    resource_group: resource_group, 
+    storage_account_name: 'starterkitstorage',
+    blob_container_name: '$web'
+  ) do
+    it { should exist }
+    its('name') { should eq('logs') }
   end
 
 end
